4aa2a3b40af7c13735b65c4590cf5ed44982e833
Jaimie <162511163+JaimieHemmings@users.noreply.github.com>
2025-06-17 15:25:53 +0100

    add: content and views
    Content added for some front end pages and started on functionality for login/out

--- store/views.py
+++ store/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from .models import Product, Category
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     products = Product.objects.order_by('-created_at')[:10] # Fetch the latest 10 products
     categories = Category.objects.all()  # Fetch all categories from the database
     return render(request, 'home.html', {'products':products, 'categories':categories})  # Render the home.html template
+
+
+def about(request):
+    return render(request, 'about.html')  # Render the about.html template
+
+
+def login_user(request):
+    return render(request, 'login.html', {})  # Render the login.html template
+
+
+def logout_user(request):
+    pass  # Placeholder for logout functionality, can be implemented later
