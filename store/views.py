39d1838673f646c450a81af1a7988ac49c3c5773
Jaimie <162511163+JaimieHemmings@users.noreply.github.com>
2025-06-18 12:47:23 +0100

    update: models

--- store/views.py
+++ store/views.py
@@ -18,7 +18,7 @@ def product(request, slug):
 
 # Create your views here.
 def home(request):
-    products = Product.objects.order_by('-created_at')[:10] # Fetch the latest 10 products
+    products = Product.objects.order_by('-created_at')[:5] # Fetch the latest 10 products
     categories = Category.objects.all()  # Fetch all categories from the database
     return render(request, 'home.html', {'products':products, 'categories':categories})  # Render the home.html template
 
