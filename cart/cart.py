497121846cbf2e6bbd34077fe851aa418075510c
Jaimie <162511163+JaimieHemmings@users.noreply.github.com>
2025-06-19 12:50:48 +0100

    add: cart summary

--- cart/cart.py
+++ cart/cart.py
@@ -1,3 +1,5 @@
+from store.models import Product
+
 class Cart():
     def __init__(self, request):
         """
@@ -14,7 +16,6 @@ class Cart():
         # Make available globally
         self.cart = cart
 
-    
     def add(self, product):
         """
         Add a product to the cart.
@@ -23,17 +24,44 @@ class Cart():
         if product.id:
             product_id = str(product.id)
             if product_id not in self.cart:
-                self.cart[product_id] = {'quantity': 1, 'price': str(product.price)}
+                self.cart[product_id] = {
+                    'quantity': 1, 'price': str(product.price)
+                }
             else:
                 self.cart[product_id]['quantity'] += 1
             self.session.modified = True
         else:
             pass
 
-
     def __len__(self):
         """
         Return the total number of items in the cart.
         :return: Total number of items in the cart.
         """
-        return len(self.cart)
+        quantity = 0
+        for product in self.cart.keys():
+            quantity += self.cart[product]['quantity']
+        return quantity
+
+    def get_prods(self):
+        """
+        Iterate over the items in the cart.
+        :return: List of products in the cart with their details.
+        """
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        # Create a list to hold products with their quantities
+        cart_products = []
+        for product in products:
+            prod_id = str(product.id)
+            cart_products.append({
+                'product': product,
+                'slug': product.slug,
+                'name': product.name,
+                'quantity': self.cart[prod_id]['quantity'],
+                'price': self.cart[prod_id]['price'],
+                'image': product.image.url if product.image else None,
+                'total': float(self.cart[prod_id]['price']) * self.cart[prod_id]['quantity']
+            })
+        return cart_products
+            
--- cart/views.py
+++ cart/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render, get_object_or_404
 from .cart import Cart
 from store.models import Product
 from django.http import JsonResponse
+from django.contrib import messages
 
 def cart_summary(request):
     """
     Render the cart summary page.
     """
-    return render(request, 'cart_summary.html', {})
+    cart = Cart(request)
+    cart_products = cart.get_prods()
+    return render(request, 'cart_summary.html', {"cart_products": cart_products})
 
 
 def cart_add(request):
@@ -20,9 +23,12 @@ def cart_add(request):
         product = get_object_or_404(Product, id=product_id)
         # Save to session
         cart.add(product=product) 
+        # Get Cart Quantity
+        cart_quantity = cart.__len__()
         # Return JSON response
+        messages.success(request, f'{product.name} has been added to your cart.')
         response = JsonResponse(
-            {'product Name: ': product.name,'success': True}
+            {'qty: ': cart_quantity}
         )      
     return response
 
