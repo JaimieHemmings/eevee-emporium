f8246e6f708fd6a63a2b518820ca804aad931185
Jaimie <162511163+JaimieHemmings@users.noreply.github.com>
2025-06-19 17:20:25 +0100

    update: nav

--- store/utils/context_processors.py
+++ store/utils/context_processors.py
@@ -1,5 +1,9 @@
-from ..models import Category
+from ..models import Category, Set
 
 def categories_processor(request):
     categories = Category.objects.all()
     return {'categories': categories}
+
+def sets_processor(request):
+    sets = Set.objects.all()
+    return {'sets': sets}
