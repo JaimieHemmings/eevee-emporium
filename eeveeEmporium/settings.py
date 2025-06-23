83669534c14fd2f87a17899cca54d6142677ae55
Jaimie <162511163+JaimieHemmings@users.noreply.github.com>
2025-06-23 19:38:39 +0100

    update: settings

--- eeveeEmporium/settings.py
+++ eeveeEmporium/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-if "USE_AWS" in os.environ:
+if "USE_AWS_S3" in os.environ:
     print("Using S3")
 
     # cache control
