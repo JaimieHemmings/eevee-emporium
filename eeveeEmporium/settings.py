f9d9ed1fb060b0ca626be5612cd7375077e8bc46
Jaimie <162511163+JaimieHemmings@users.noreply.github.com>
2025-06-23 18:53:26 +0100

    update: requirements

--- eeveeEmporium/settings.py
+++ eeveeEmporium/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import os
+from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,12 +11,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@d@2)sbii)s4!ygxpb_h)pgs)q)-48n#$c!pflhmulgemq%18='
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    ".herokuapp.com",
+    '127.0.0.1',
+    'localhost',
+    ]
 
 
 # Application definition
@@ -72,10 +79,7 @@ WSGI_APPLICATION = 'eeveeEmporium.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(config('DATABASE_URL'))
 }
 
 
