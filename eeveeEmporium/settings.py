2dccea4ab1a356fe32f47bc085d96aeb108305f7
Jaimie <162511163+JaimieHemmings@users.noreply.github.com>
2025-06-24 18:20:37 +0100

    Update settings.py

--- eeveeEmporium/settings.py
+++ eeveeEmporium/settings.py
@@ -21,7 +21,7 @@ if DEBUG_STATE != True:
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
 
     # Default "from" email address
     DEFAULT_FROM_EMAIL = 'noreply@eeveeemporium.com'
